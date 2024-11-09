2a7146ee5b2448814d7a555bc9c71fbbc37b372d
kerfernandez <kerboyfernandez2@gmail.com>
2024-11-09 10:09:08 -0500

    Update CommissionEmployee
    Update CommissionEmployee

--- CommissionEmployee.php
+++ CommissionEmployee.php
@@ -1,45 +1,30 @@
 <?php
 
-require_once 'Employee.php';
+class CommissionEmployee {
+    private string $name;
+    private string $address;
+    private int $age;
+    private string $companyName;
+    private float $regularSalary;
+    private int $itemsSold;
+    private float $commissionRate;
 
-class CommissionEmployee extends Employee {
-    public function __construct(
-        string $name, 
-        string $address, 
-        int $age, 
-        string $companyName, 
-        private float $regularSalary, 
-        private int $itemSold, 
-        private float $commissionRate
-    ) {
-        parent::__construct($name, $address, $age, $companyName);
-        $this->commissionRate /= 100;  
+    public function __construct($name, $address, $age, $companyName, $regularSalary, $itemsSold, $commissionRate) {
+        $this->name = $name;
+        $this->address = $address;
+        $this->age = $age;
+        $this->companyName = $companyName;
+        $this->regularSalary = $regularSalary;
+        $this->itemsSold = $itemsSold;
+        $this->commissionRate = $commissionRate;
     }
 
-    public function getRegularSalary(): float {
-        return $this->regularSalary;
+    public function getName() {
+        return $this->name;
     }
 
-    public function getItemSold(): int {
-        return $this->itemSold;
-    }
-
-    public function getCommissionRate(): float {
-        return $this->commissionRate;
-    }
-
-    public function earnings(): float {
-        return $this->regularSalary + ($this->itemSold * $this->commissionRate);
-    }
-
-    public function __toString(): string {
-        return sprintf(
-            "%s, Regular Salary: %.2f, Items Sold: %d, Commission Rate: %.2f", 
-            parent::__toString(), 
-            $this->regularSalary, 
-            $this->itemSold, 
-            $this->commissionRate
-        );
+    public function calculatePay() {
+        return $this->regularSalary + ($this->itemsSold * $this->commissionRate / 100);
     }
 }
 ?>
